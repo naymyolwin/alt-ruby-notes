61c99ac38dd1dea1b8857bac652f1be0735f8e55
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-01 21:24:50 +0700

    update notes

---
+++ ruby.rb
@@ -0,0 +1,56 @@
+### String ###
+# fn = "nay myo"
+# ln = "lwin"
+
+# available methods
+# puts fn.methods
+# puts fn.class
+
+## substitute
+# puts fn.sub("myo", "nay")
+# puts fn.gsub("myo", "nay")
+
+# first_name = "Nay"
+# new_first_name = first_name
+
+# first_name = "Nay Nay"
+# puts new_first_name 
+## even tho they are object, first_name is reassign to new location memory. not the same as JS
+
+## \ (escape character)
+## getting user input
+# input = gets.chomp
+## gets.chomp get you string
+# puts "hello #{input}"
+
+### Number ###
+# puts 1 + 2
+# 20.times {puts "hello"}
+# puts rand(10)
+
+### comparison operator ###
+# ==
+# !=
+# >
+# <
+# .eql?() will compare type
+
+# puts 10 === "10".to_f
+
+### Methods ###
+# num1 = 3
+# num2 = 4
+
+# def multiply (num1, num2)
+#     num1.to_f * num2.to_f #the last line will automatically return or can put return keyword
+# end
+
+# puts multiply(num1,num2)
+
+### Branching (conditionals) ###
+# if true
+#     puts "true"
+# else puts "false"
+# end
+
+
